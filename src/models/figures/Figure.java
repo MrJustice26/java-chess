8a119cd32ef1f6e164211de596321e2e3bca2209
s481809 <wlakuc@st.amu.edu.pl>
2023-04-30 15:31:44 +0200

    * CHE - Add movedStates, receivedMoveHistoryStates and relevant methods for them.

--- src/models/figures/Figure.java
+++ src/models/figures/Figure.java
@@ -9,6 +9,25 @@ public class Figure {
 
     private String imageName;
 
+    // TODO Remove it and use MovedStates
+    private boolean hasMoved;
+
+    enum MovedStates {
+        NOT_MOVED,
+        MOVED_FROM_START,
+        MOVED_FROM_NOT_START
+    }
+
+    enum ReceivedMoveHistoryStates {
+        THIS_ROUND,
+        PREVIOUS_ROUND,
+        OLDER_THAN_PREVIOUS_ROUND
+    }
+
+    MovedStates movedState = MovedStates.NOT_MOVED;
+
+    ReceivedMoveHistoryStates receivedMoveHistoryState = ReceivedMoveHistoryStates.THIS_ROUND;
+
     public Figure(Colors color, Cell cell){
         this.color = color;
         this.cell = cell;
@@ -47,10 +66,61 @@ public class Figure {
         if(target.getFigure().getColor() == this.getColor()) {
             return false;
         }
-        if(target.getFigure().getName() == FigureNames.KING){
-            return false;
-        }
-        return true;
+        return (target.getFigure().getName() != FigureNames.KING);
     }
+
+    public boolean getHasMoved(){ return this.hasMoved;}
+    public void setIsMoved(boolean moveState){
+        this.hasMoved = moveState;
+    }
+
+    public MovedStates getMovedState(){
+        return this.movedState;
+    }
+
+    public void setMovedState(MovedStates movedState){
+        this.movedState = movedState;
+    }
+
+    public void changeMovedState(){
+        switch(this.getMovedState()){
+            case NOT_MOVED:
+                this.setMovedState(MovedStates.MOVED_FROM_START);
+                break;
+            case MOVED_FROM_START:
+                this.setMovedState(MovedStates.MOVED_FROM_NOT_START);
+                break;
+        }
+    }
+
+    public void changeReceivedMoveHistoryState(){
+        switch(this.receivedMoveHistoryState){
+            case THIS_ROUND:
+                this.receivedMoveHistoryState = ReceivedMoveHistoryStates.PREVIOUS_ROUND;
+                break;
+            case PREVIOUS_ROUND:
+                this.receivedMoveHistoryState = ReceivedMoveHistoryStates.OLDER_THAN_PREVIOUS_ROUND;
+                break;
+            case OLDER_THAN_PREVIOUS_ROUND:
+                break;
+            default:
+                System.out.println("Undefined receivedMoveHistoryState!!!");
+                break;
+        }
+
+    }
+
+    public void markAsUpdatedMoveHistoryState(){
+        this.receivedMoveHistoryState = ReceivedMoveHistoryStates.THIS_ROUND;
+    }
+
+    public boolean isMovedPreviousRound(){
+        return this.receivedMoveHistoryState == ReceivedMoveHistoryStates.PREVIOUS_ROUND;
+    }
+
+    public boolean isMovedFromStart(){
+        return this.getMovedState() == MovedStates.MOVED_FROM_START;
+    }
+
 
 }
