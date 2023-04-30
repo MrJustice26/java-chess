a17209db9cb5253b09582c29c066e7b48810c63b
s481809 <wlakuc@st.amu.edu.pl>
2023-04-30 02:09:58 +0200

    Code refactor and code improvement

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -2,13 +2,14 @@ package gui;
 
 import models.Board;
 import models.Cell;
-import models.Colors;
 import models.figures.Figure;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Gui {
+
+    Color greenColor = new Color(116, 217, 38);
 
     Board board;
     String assetsFolderPath = "assets/";
@@ -28,23 +29,7 @@ public class Gui {
         JPanel panel = new JPanel(new GridLayout(8, 8));
         panel.setBackground(Color.white);
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                Cell cell = this.board.getCell(j, i);
-                JButton button = new JButton();
-                if(!cell.isEmpty()){
-                    Figure cellFigure = cell.getFigure();
-                    String pathToFigureImage = this.assetsFolderPath + cellFigure.getImageName();
-                    ImageIcon icon = new ImageIcon(pathToFigureImage);
-                    button.setIcon(icon);
-                }
-                this.applyButtonOptions(button);
-                button.putClientProperty("cell", cell);
-                button.addActionListener(e -> actionButtonScript(button));
-                button.setBackground((i + j) % 2 == 0 ? Color.white : Color.gray);
-                panel.add(button);
-            }
-        }
+        this.renderButtons(panel);
 
         frame.add(panel);
         frame.setVisible(true);
@@ -58,7 +43,53 @@ public class Gui {
         buttonInstance.setRolloverEnabled(false);
     }
 
-    private void actionButtonScript(JButton button){
+    private void deleteButtons(JPanel panel){
+        for (Component component : panel.getComponents()) {
+            if (component instanceof JButton) {
+                JButton button = (JButton) component;
+                panel.remove(button);
+            }
+        }
+    }
+    private void renderButtons(JPanel panel){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Cell cell = this.board.getCell(j, i);
+                JButton button = new JButton();
+                if(!cell.isEmpty()){
+                    Figure cellFigure = cell.getFigure();
+                    String pathToFigureImage = this.assetsFolderPath + cellFigure.getImageName();
+                    ImageIcon icon = new ImageIcon(pathToFigureImage);
+                    button.setIcon(icon);
+                }
+                this.applyButtonOptions(button);
+                button.putClientProperty("cell", cell);
+                button.addActionListener(e -> this.actionButtonScript(button, panel));
+                panel.add(button);
+            }
+        }
+        this.drawDefaultBackgroundButtonsColor(panel);
+    }
+
+    private void reRenderButtons(JPanel panel){
+        this.deleteButtons(panel);
+        this.renderButtons(panel);
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    private void drawDefaultBackgroundButtonsColor(JPanel panel){
+        for (Component component : panel.getComponents()) {
+            if (component instanceof JButton) {
+                JButton button = (JButton) component;
+                Cell cell = (Cell) button.getClientProperty("cell");
+                Color cellColor = (cell.getX() + cell.getY()) % 2 == 0 ? Color.white : Color.gray;
+                button.setBackground(cellColor);
+                button.setBorderPainted(false);
+            }
+        }
+    }
+    private void actionButtonScript(JButton button, JPanel panel){
         Cell cell = (Cell) button.getClientProperty("cell");
 
         switch(moveState){
@@ -68,23 +99,33 @@ public class Gui {
                 button.setBackground(Color.darkGray);
                 moveState = MoveStates.SELECTED;
                 selectedButtonInstance = button;
+
+                boolean[][] availableMoves = cell.getAvailableMoves();
+                for(int y = 0; y < 8; y++){
+                    for(int x = 0; x < 8; x++){
+                        if(availableMoves[y][x]){
+                            Component component = panel.getComponent(y * 8 + x);
+                            if (component instanceof JButton) {
+                                JButton iteratedButton = (JButton) component;
+                                iteratedButton.setBackground(this.greenColor);
+                                iteratedButton.setBorderPainted(true);
+                            }
+                        }
+                    }
+                }
                 break;
             case SELECTED:
                 Cell selectedCell = (Cell) selectedButtonInstance.getClientProperty("cell");
-                Color selectedCellColor = selectedCell.getFigure().getColor() == Colors.BLACK ? Color.gray : Color.white;
                 if(selectedCell == cell){
-                    System.out.println("Same piece clicked");
-                    System.out.println(selectedCellColor);
-                    selectedButtonInstance.setBackground(selectedCellColor);
+                    this.drawDefaultBackgroundButtonsColor(panel);
+                    moveState = MoveStates.NOT_SELECTED;
                     break;
                 }
                 if(selectedCell.getFigure().canMove(cell)){
-                    System.out.println("Piece can go to");
                     moveState = MoveStates.NOT_SELECTED;
                     selectedButtonInstance.setBackground(Color.black);
-                } else {
-                    System.out.println("Piece can't go");
-                    moveState = MoveStates.NOT_SELECTED;
+                    selectedCell.moveFigure(cell);
+                    reRenderButtons(panel);
                 }
 
 
